2ac38348d28e75410790df9d1ef9a0957ecab214
Martin Klapper <martinklapper@tutanota.com>
2023-06-06 13:03:28 +0200

    initial model relations

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,3 +1,4 @@
 class ShoppingList < ApplicationRecord
   belongs_to :user
+  has_many :ingredients, through: :shopping_list_ingredients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_one :inventory
+  has_one :shopping_list
+  has_many :recipes
+
+  has_many :to_be_cookeds
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
