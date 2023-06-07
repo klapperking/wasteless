ed9ed86b13492b62824bfc09d6cfec6413931c98
Martin Klapper <martinklapper@tutanota.com>
2023-06-07 11:58:03 +0200

    bugfix email regex validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,11 @@
 class Recipe < ApplicationRecord
+  # relations
   belongs_to :user
   has_many :ingredients, through: :recipe_ingredients
+
+  # validations
+  validates :user, presence: true
+  validates :name, presence: true
+  validates :desription
+  validates :number_of_people
 end
