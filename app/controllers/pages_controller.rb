c4691fcd06d3388f74f60384b6f784e8ef185b01
Florian Guerin <guerinflorian02@gmail.com>
2023-06-07 16:34:31 +0200

    recommended part home page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @ingredients = Ingredient.all
+    @my_pets = { "Less than 2 days" => 12, "2 - 4 days" => 7, "5 - 7 days" => 5, "1 - 2 weeks" => 8, "2 - 4 weeks" => 13, "More than a month" => 6 }
   end
 end
