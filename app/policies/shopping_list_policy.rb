4e2b3e0238c8f92364b901dbce44905ed564027a
Florian Guerin <guerinflorian02@gmail.com>
2023-06-08 17:06:52 +0200

    shopping list done, need to fix the qty

--- app/policies/shopping_list_policy.rb
+++ app/policies/shopping_list_policy.rb
@@ -5,4 +5,8 @@ class ShoppingListPolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def show?
+    record.user == user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :inventory_ingredients, only: %i[edit update destroy]
 
+  resources :shopping_lists, only: [:show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
