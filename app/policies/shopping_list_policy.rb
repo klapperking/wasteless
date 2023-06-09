a9cdd1d5a7bb0fe93031d671f256a846d61ad398
Martin Klapper <martinklapper@tutanota.com>
2023-06-09 10:19:20 +0200

    working version of inventory and shopping-list

--- app/policies/shopping_list_policy.rb
+++ app/policies/shopping_list_policy.rb
@@ -1,12 +1,12 @@
 class ShoppingListPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all.find_by(user:)
+    end
   end
 
   def show?
-    record.user == user
+    true
   end
 end
